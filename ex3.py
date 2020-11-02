9cd950a1ba49b7b92ab76d7ae7954b3c017025e2
salamanderjones <73806214+salamanderjones@users.noreply.github.com>
2020-11-02 14:01:33 -0600

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,23 @@
+print("I will now count my chickens:") # This simply prints the line
+
+print("Hens", 25 + 30 / 6) # This does math and shows up as 30
+print("Roosters", 100 - 25 * 3 % 4) # This does math and shows up as 97
+
+print("Now I will count the eggs:") #This simply prints the line
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6) #This does the math and shows up as 7
+
+print("Is it true that 3 + 2 < 5 - 7?") #This just prints the line
+
+print(3 + 2 < 5 - 7) #This says false, because the math isn't true
+
+print("What is 3 + 2?", 3 + 2) #This prints the first line, then shows up as 5
+print("What is 5 - 7?", 5 - 7) #This prints the first line, and shows up as -2
+
+print("Oh, that's why it's False.") #simply prints
+
+print("How about some more.") #simply prints
+
+print("Is it greater?", 5 > -2) #prints and says True, because that math is true
+print("Is it greater or equal?", 5 >= -2) #prints and says True, because that math is true
+print("Is it less or equal?", 5 <= -2) #prints and says false, because the math is off
