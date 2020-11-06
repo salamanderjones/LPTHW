5e3d8d5471263bd0ef4527955ef401edf1e5e8b2
salamanderjones <73806214+salamanderjones@users.noreply.github.com>
2020-11-06 15:43:14 -0600

    Add files via upload

---
+++ ex6.py
@@ -0,0 +1,17 @@
+types_of_people = 10 #this is a variable and a value assinged to it
+x =f"There are {types_of_people} types of people." #variable with a stringe
+binary = "binary"
+do_not = "don't"
+y = f"Those who know {binary} and those who {do_not}."
+
+print(x)
+print(y)
+
+print(f"I said: {x}")
+print(f"I also said: '{y}'")
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! {}"
+print(joke_evaluation.format(hilarious))
+w = "This is the left side of..."
+e = "a string with a right side."
+print(w + e)
